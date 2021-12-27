01c2bb74e54dd638a6394efaf0236813d1695546
hi-tech-jazz <eriklovmojonsson@gmail.com>
2021-12-27 11:22:12 +0100

    tests: add more tree grower tests

--- spec/hybridforest/trees/tree_growers/id3_grower_spec.rb
+++ spec/hybridforest/trees/tree_growers/id3_grower_spec.rb
@@ -3,6 +3,22 @@
 RSpec.describe HybridForest::Trees::TreeGrowers::ID3Grower do
   subject(:tree_grower) { described_class.new }
 
+  describe "#grow_tree(instances)" do
+    let(:instances) do
+      Rover::DataFrame.new(
+        {
+          a: [1, 2, 4, 5, 6, 1],
+          b: %w[aaa aa bb aa bb bbb],
+          label: [1, 0, 0, 1, 1, 1]
+        }
+      )
+    end
+    it "grows a tree and returns the root node" do
+      root = tree_grower.grow_tree(instances)
+      expect(root).to respond_to(:classify)
+    end
+  end
+
   describe "#potential_split_values(feature, instances)" do
     let(:instances) do
       Rover::DataFrame.new([
